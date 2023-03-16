fc0a8dc3f80052603fab97109a2cbdff9316e762
Diyor <dierhalilov33@gmail.com>
2023-03-16 16:42:08 +0500

    initial

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request: WSGIRequest) -> HttpResponse:
+    ip = request.META['REMOTE_ADDR']
+    return render(
+        request,
+        'core/pages/index.html',
+    )
